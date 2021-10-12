3672bd5f71fa8722e3400e36a917564170598ad5
andyantunes <andersonlantunes@gmail.com>
2021-10-12 16:21:31 -0300

    Created stock-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StockListComponent } from './components/stock-list/stock-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockListComponent
   ],
   imports: [
     BrowserModule
